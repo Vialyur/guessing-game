16bc0616adaf9853a2d111e8cc4ba61a42f0659b
Vital Yurau <vialyur@list.ru>
2019-10-13 21:19:42 +0300

    feat: add solution of the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.goal = null;
+        this.minValue = null;
+        this.maxValue = null;
+
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return this.goal = Math.ceil((this.minValue + this.maxValue) / 2);
     }
 
     lower() {
-
+        this.maxValue = this.goal;
     }
 
     greater() {
-
+        this.minValue = this.goal;
     }
 }
+
 
 module.exports = GuessingGame;
